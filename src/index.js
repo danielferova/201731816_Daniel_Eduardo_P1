e693b28efa5cedc7e8a1f294e586ec5302345b32
danielferova <danielferova9@hotmal.com>
2019-10-03 23:41:00 -0600

    Segundo commit proyecto1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("DANIEL IMBECIL");
+//imports para el servidor
+const express = require ('express');
+const app = express();
+
+
+//configuracion del puerto
+app.set('port', 3000);
+
+
+//rutas
+app.get('/', (req, res) => {
+    res.send('<h1> Hola mundo <h1>');
+})
+
+app.get('/hola', (req, res) => {
+    res.sendFile(__dirname+'/views/proyecto1.html');
+}
+)
+
+//para escuchar el puerto
+app.listen(app.get('port'),() => {
+//console.log(__dirname);
+console.log('escuchando en el puerto', app.get('port'));
+});
