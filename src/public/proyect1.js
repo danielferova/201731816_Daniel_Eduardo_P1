da594c056496d5b331415205999448707cf0b584
danielferova <danielferova9@hotmal.com>
2019-10-11 03:25:05 -0600

    Archivo de texto

---
+++ src/public/proyect1.js
@@ -0,0 +1,22 @@
+function leerArchivo(e) {
+    var archivo = e.target.files[0];
+    if (!archivo) {
+      return;
+    }
+    var lector = new FileReader();
+    lector.onload = function(e) {
+      var contenido = e.target.result;
+      mostrarContenido(contenido);
+    };
+    lector.readAsText(archivo);
+  }
+  
+  function mostrarContenido(contenido) {
+    
+    var elemento = document.getElementById('contenido-archivo');
+    // formulario.innerHTML = "<input type=\"submit\" name=\"bote\" value=\"Analizar\"/><br>"+"</form>";
+    elemento.innerHTML =elemento.innerHTML+  "<input type=\"submit\" name=\"bote\" value=\"Analizar\"/><br>"+"</form>"+contenido;
+  }
+  
+  document.getElementById('file-input')
+    .addEventListener('change', leerArchivo, false);
